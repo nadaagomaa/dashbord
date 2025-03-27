ad3b0c5215729e0bc8a9b32e77374b48e521c4c3
nadaagomaa <nada.swidaan@gmail.com>
2025-03-27 12:45:30 +0200

    modify html ,scss and js files

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-document.querySelectorAll('.icon-link,.sidebar-link').forEach(link => {
+document.querySelectorAll('.icon-link,.nav-link,.icon-btn').forEach(link => {
     link.addEventListener('click', function() {
-        document.querySelectorAll('.icon-link,.sidebar-link').forEach(item => {
+        document.querySelectorAll('.icon-link,.nav-link,.icon-btn').forEach(item => {
             item.classList.remove('clicked');
         });
         this.classList.add('clicked');
     });
 });
-const sidebarLinks = document.querySelectorAll('.sidebar-link');
+const sidebarLinks = document.querySelectorAll('.nav-link');
     sidebarLinks.forEach(link => {
         link.addEventListener('click', function(e) {
             e.preventDefault();
-            const clickedTitle = this.querySelector('.title').textContent || this.querySelector('.title').innerText;
+            const clickedTitle = this.querySelector('.link-text').textContent || this.querySelector('.title').innerText;
             document.getElementById('header-title').textContent = clickedTitle;
             
         });
